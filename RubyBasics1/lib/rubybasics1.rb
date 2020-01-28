39baf5c4cc7f6303d3765cbdf71f2be442686f11
EC2 Default User <ec2-user@ip-172-31-41-158.ec2.internal>
2020-01-28 19:49:41 +0000

    RubyBasics1- Part 3 v2

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -23,17 +23,25 @@ end
 
 # Part III
 def sum_to_n? arr, n
-  sumToN = false
-  if arr.size == 0
-    return sumToN
+  if (arr.length == 0)
+    return false
   end
-  if arr.size == 1
-    return sumToN
+  if (arr.length == 1)
+    return false
   end
-  if arr.combination(2).detect{|a,b| a + b == n} != nil
-    sumToN = true
-    return sumToN
-  else 
-  return sumToN
+  sorted_arr = arr.sort
+  head =0
+  tail = sorted_arr.length-1
+  
+  while head < tail 
+    current_sum = sorted_arr[head] + sorted_arr[tail]
+    if current_sum == n
+      return true
+    elsif current_sum < n
+      head = head + 1
+    else 
+      tail = tail - 1
+    end
   end
+  return false
 end
