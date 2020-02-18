a5df628de56887b37abc6e68b2f383c8d99671cb
EC2 Default User <ec2-user@ip-172-31-41-158.ec2.internal>
2020-02-18 20:18:05 +0000

    Ruby Basics 2 - Part 2

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -8,6 +8,14 @@ end
 # Part II
 def starts_with_consonant? s
   # YOUR CODE HERE
+  start = s.chars[0]
+  if s.empty?
+    return false
+  elsif start.match(/[aeiouAEIOU]/) || start.match(/[^a-zA-Z]/)
+      return false
+  else
+    return true
+  end
 end
 
 # Part III
