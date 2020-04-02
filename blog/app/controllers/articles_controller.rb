4363b46e1be4b0bbde7a0ea7b7310b4232d84e87
EC2 Default User <ec2-user@ip-172-31-41-158.ec2.internal>
2020-04-02 13:19:45 +0000

    Add Form Partial and Update the New view

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -14,7 +14,9 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         
+        
         if @article.save
+            
             redirect_to @article
         else 
             render 'new'
